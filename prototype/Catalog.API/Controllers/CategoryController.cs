0219b3fcbea8405d051d18d28025f204e4546199
Mario Palacios <mario@Marios-iMac.local>
2021-05-25 13:14:58 -0400

    Catalog.API: finished Services and Controllers

--- prototype/Catalog.API/Controllers/CategoryController.cs
+++ prototype/Catalog.API/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using Catalog.API.Models;
@@ -11,19 +12,11 @@ namespace Catalog.API.Controllers
     [Route("api/v1/[controller]")]
     public class CategoryController : ControllerBase
     {
-        private ICategoryService _categoryService;
+        private readonly ICategoryService _categoryService;
 
         public CategoryController(ICategoryService categoryService)
         {
             _categoryService = categoryService ?? throw new ArgumentNullException(nameof(categoryService));
-        }
-
-        [HttpPost]
-        [Route(null, Name = "Create")]
-        [ProducesResponseType(typeof(CategoryModel), (int)HttpStatusCode.Created)]
-        public async Task<ActionResult<CategoryModel>> Create([FromBody] CategoryModel model)
-        {
-            return Ok(await _categoryService.Create(model));
         }
 
         [HttpGet]
@@ -34,6 +27,37 @@ namespace Catalog.API.Controllers
             return Ok(await _categoryService.GetById(id));
         }
 
-        
+        [HttpGet]
+        [Route(null, Name = "GetAll")]
+        [ProducesResponseType(typeof(IEnumerable<ProductModel>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<ProductModel>>> GetAll()
+        {
+            return Ok(await _categoryService.GetAll());
+        }
+
+        [HttpPost]
+        [Route(null, Name = "Create")]
+        [ProducesResponseType(typeof(CategoryModel), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult<CategoryModel>> Create([FromBody] CategoryModel model)
+        {
+            return Ok(await _categoryService.Create(model));
+        }
+
+        [HttpPut]
+        [Route(null, Name = "Modify")]
+        [ProducesResponseType(typeof(ProductModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<CategoryModel>> Modify([FromBody] CategoryModel model)
+        {
+            return Ok(await _categoryService.Modify(model));
+        }
+
+        [HttpDelete]
+        [Route("{id}", Name = "Delete")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            return Ok(await _categoryService.DeleteById(id));
+        }
+
     }
 }
---
+++ prototype/Catalog.API/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Catalog.API.Models;
+using Catalog.API.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetById")]
+        [ProducesResponseType(typeof(ProductModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ProductModel>> GetById(int id)
+        {
+            return Ok(await _productService.GetById(id));
+        }
+
+        [HttpGet]
+        [Route("{categoryId}", Name = "GetByCategoryId")]
+        [ProducesResponseType(typeof(IEnumerable<ProductModel>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<ProductModel>>> GetByCategoryId(int categoryId)
+        {
+            return Ok(await _productService.GetByCategoryId(categoryId));
+        }
+
+        [HttpGet]
+        [Route(null, Name = "GetAll")]
+        [ProducesResponseType(typeof(IEnumerable<ProductModel>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<ProductModel>>> GetAll()
+        {
+            return Ok(await _productService.GetAll());
+        }
+
+        [HttpPost]
+        [Route(null, Name = "Create")]
+        [ProducesResponseType(typeof(ProductModel), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult<ProductModel>> Create([FromBody] ProductModel model)
+        {
+            return Ok(await _productService.Create(model));
+        }
+
+        [HttpPut]
+        [Route(null, Name = "Modify")]
+        [ProducesResponseType(typeof(ProductModel), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ProductModel>> Modify([FromBody] ProductModel model)
+        {
+            return Ok(await _productService.Modify(model));
+        }
+
+        [HttpDelete]
+        [Route("{id}", Name = "Delete")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            return Ok(await _productService.DeleteById(id));
+        }
+
+    }
+}
