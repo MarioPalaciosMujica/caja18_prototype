2975a24a326e10800646a5792e78f03b81be65a9
Mario Palacios <mario@Marios-iMac.local>
2021-05-28 10:24:41 -0400

    IS4: created project with basic nugets and configurations

---
+++ prototype/IdentityServer/Config.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("API.name", "API.description")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedScopes = { "API.name" }
+                }
+            };
+        }
+
+    }
+
+
+}
